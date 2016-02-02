2f58dd690450aaf022b69b94ba53217715f7921b
DylanCh <hchen004@citymail.cuny.edu>
2016-02-02 12:08:49 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Threading;
+
 namespace MatrixMultiplicationPerformanceTimer{
   public class program {
     public static void Main(){
+      Matrix matrix = new Matrix();
+      matrix.populate();
       
     }
   }
