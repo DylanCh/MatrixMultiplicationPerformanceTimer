3e66f681e9e76c7c86521ec1757d4142ebd32471
DylanCh <hchen004@citymail.cuny.edu>
2016-02-02 13:23:49 -0500

    Update Matrix.cs

--- Matrix.cs
+++ Matrix.cs
@@ -6,10 +6,15 @@ namespace MatrixMultiplicationPerformanceTimer{
     public int y;
     public int m;
     public int n;
+    public double[][] a;
+    public double[][] b;
     
     // Default: 8x8 matrix
     public Matrix(){
       x=y=m=n=8;
+      //https://msdn.microsoft.com/en-us/library/aa287601(v=vs.71).aspx
+      a = new double[x][y];
+      b = new double[m][n];
     }
     
     // Customize matrix
@@ -20,23 +25,24 @@ namespace MatrixMultiplicationPerformanceTimer{
       this.n = n;
     }
     
-    public void multiplication(int** c,int** b,int** a){
+    public double[][] multiplication (){
+    	double result[x][n];
 	    for (int i = 0; i<x; i++){
 		    for (int j = 0; j<n; j++){
-			    c[i][j] = 0;
+			    result[i][j] = 0;
 			    for (int k = 0; k<m; k++){
-				    c[i][j] = c[i][j] + a[i][k] * b[k][j];
+				    result[i][j] = result[i][j] + a[i][k] * b[k][j];
 			    }
 		    }
 	    }
+	    return result;
     }
     
-    public void populate(){
-    	for (int row = 0; row <x; j++){
-		for (int col = 0; col<n; k++){
-			a[col][row]=2;
-			b[col][row]=3;
-		}
+    public static void populate(int x,int n, double matrix){
+    	for (int row = 0; row < matrix.getLength(0); j++){
+		for (int col = 0; col < matrix.getLength(1); k++){
+			matrix[col][row]=2;
+			}
 	}
     }
   }
