521b65970a48c1173856daf67decf36c3bd84b5b
Dylan Chen <hchen004@citymail.cuny.edu>
2016-02-06 13:01:27 -0500

    Update Matrix.cs

--- Matrix.cs
+++ Matrix.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System;
 
 namespace MatrixMultiplicationPerformanceTimer{
@@ -13,22 +16,24 @@ namespace MatrixMultiplicationPerformanceTimer{
     public Matrix(){
       x=y=m=n=8;
       //https://msdn.microsoft.com/en-us/library/aa287601(v=vs.71).aspx
-      a = new double[x][y];
-      b = new double[m][n];
+      a = new double[x][];
+      b = new double[m][];
     }
     
     // Customize matrix
-    public Matrix(int x, iny, int m, int n){
+    public Matrix(int x, int y, int m, int n){
       this.x = x;
       this.y = y;
       this.m = m;
       this.n = n;
+      a = new double[x][];
+      b = new double[m][];
     }
     
     public double[][] multiplication (){
-    	double result[x][n];
-	    for (int i = 0; i<x; i++){
-		    for (int j = 0; j<n; j++){
+    	double[][] result = new double[x][];
+	    for (int i = 0; i < x; i++){
+		    for (int j = 0; j < n; j++){
 			    result[i][j] = 0;
 			    for (int k = 0; k<m; k++){
 				    result[i][j] = result[i][j] + a[i][k] * b[k][j];
@@ -38,12 +43,16 @@ namespace MatrixMultiplicationPerformanceTimer{
 	    return result;
     }
     
-    public static void populate(double matrix){
-    	for (int row = 0; row < matrix.getLength(0); j++){
-		for (int col = 0; col < matrix.getLength(1); k++){
-			matrix[col][row]=2;
+    public static void populate(double[][] matrix){
+    	for (int row = 0; row < matrix.GetLength(0); row++){
+		    for (int col = 0; col < matrix.GetLength(1); col++){
+			    matrix[col][row]=2;
 			}
-	}
+	    }
+    } // end method
+
+    public void print() { 
+        
     }
   }
 }
